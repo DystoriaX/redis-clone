98b695c816438ce0fe9a0705833d35cc59ea677f
DystoriaX <dascogabriel@gmail.com>
2023-07-15 16:06:08 +0800

    server.cpp: add missing listen() syscall

--- server.cpp
+++ server.cpp
@@ -4,11 +4,6 @@
 #include <sys/socket.h>
 #include <netinet/ip.h>
 
-void die(const char* msg) {
-  std::cerr << msg << "\n";
-  exit(1);
-}
-
 void echo(int connfd) {
   char rbuf[64] = {};
 
@@ -19,7 +14,7 @@ void echo(int connfd) {
     return;
   }
 
-  std::cout << "client says: " << rbuf;
+  std::cout << "client says: " << rbuf << "\n";
 
   write(connfd, rbuf, strlen(rbuf));
 }
@@ -38,16 +33,21 @@ int main() {
   addr.sin_port = ntohs(1234);
   addr.sin_addr.s_addr = ntohl(0);
 
-  int rv = bind(fd, (const sockaddr*) &addr, sizeof(addr));
+  if (bind(fd, (const sockaddr*) &addr, sizeof(addr))) {
+    perror("bind()");
+  }
 
-  if (rv) {
-    die("bind()");
+  if (listen(fd, SOMAXCONN)) {
+    perror("listen()");
   }
 
   while (true) {
     sockaddr_in client_addr = {};
     socklen_t socklen = sizeof(client_addr);
+
+    std::cout << "listening...\n";
     int connfd = accept(fd, (struct sockaddr*) &client_addr, &socklen);
+    std::cout << "new client accepted\n";
 
     if (connfd < 0) {
       continue;
